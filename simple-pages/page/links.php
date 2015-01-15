3c9b27e9d3442c45c9b1f4a3b152e4197768c691
HafizHanif <apitlekays@gmail.com>
2015-01-15 14:04:05 +0000

    Ported Links page into PHP

--- simple-pages/page/links.php
+++ simple-pages/page/links.php
@@ -1,8 +1,32 @@
-<?php 
-
-echo "This is links page";
-
-
-
-
-?>
+<div class="page-links">
+<section class="masthead clearfix">
+    <h2 class="masthead-links">Links</h2>
+</section>
+  <section class="links-page clearfix">
+    <div class="external-links-contents clearfix">
+      <h2 class="links-title">Link Header 1</h2>
+      <div class="external-links clearfix">
+        <ul class="clearfix">
+          <li class="clearfix">
+            <a href="#">link 1</a>
+          </li>
+        </ul>
+        <ul class="_container-12 clearfix">
+          <li class="clearfix">
+            <a href="#">link 1</a>
+          </li>
+        </ul>
+        <ul class="_container-15 clearfix">
+          <li class="clearfix">
+            <a href="#">link 1</a>
+          </li>
+        </ul>
+        <ul class="_container-19 clearfix">
+          <li class="clearfix">
+            <a href="#">link 1</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</div>
