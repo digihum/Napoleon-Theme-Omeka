554e9a0f66305d99c229902a315c23835479adb8
HafizHanif <apitlekays@gmail.com>
2015-01-15 13:52:01 +0000

    Ported Events page into PHP

---
+++ simple-pages/page/events.php
@@ -0,0 +1,25 @@
+<div class="page-events">
+<section class="masthead clearfix">
+    <h2 class="masthead-events">Upcoming Events</h2>
+</section>
+  <section class="events events-2 clearfix">
+    <div class="event-contents clearfix">
+      <div class="calendar">
+        <div class="month-bg clearfix">
+          <p class="month">FEB</p>
+        </div>
+        <div class="date-bg clearfix">
+          <p class="date">20</p>
+        </div>
+      </div>
+      <div class="event-node">
+        <div class="event-details">
+          <p>Claritatem insitam est usus legentis in iis qui facit eorum claritatem Investigationes demonstraverunt lectores legere. Hendrerit in vulputate velit esse molestie consequat vel illum; dolore eu feugiat nulla.</p>
+          <p>Eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim. Quinta decima eodem modo typi qui nunc nobis videntur parum, clari fiant sollemnes. Nam liber tempor cum soluta nobis assum typi. Nulla facilisis at vero eros et accumsan et iusto odio dignissim qui. Eum iriure dolor in hendrerit in vulputate velit esse. Etiam processus dynamicus qui sequitur mutationem consuetudium lectorum mirum est. Claritatem Investigationes demonstraverunt lectores legere me lius, quod ii legunt.</p>
+        </div>
+        <h2 class="event-title">Title of the event</h2>
+      </div>
+      <button class="signup">Sign Up</button>
+    </div>
+  </section>
+</div>
