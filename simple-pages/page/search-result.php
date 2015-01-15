fd7a96cc72a0ed23df67324eb8067349fcf5be95
HafizHanif <apitlekays@gmail.com>
2015-01-15 14:36:34 +0000

    Ported search result page into PHP

--- simple-pages/page/search-result.php
+++ simple-pages/page/search-result.php
@@ -1,8 +1,9 @@
-<?php 
-
-echo "This is search result page";
-
-
-
-
-?>
+<div class="page-search-result clearfix">
+<section class="masthead clearfix">
+    <h2 class="masthead-searchresults">Search Results</h2>
+ </section>
+  <section class="searchresults clearfix">
+    <input class="sitewide-search" placeholder="search for an item here" type="text">
+    <p> Here lies the results of search</p>
+  </section>
+</div>
