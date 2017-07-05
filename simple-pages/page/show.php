13099d2935050ff58fa013ecce1acf427d584e4b
Ranford <aosgao@amabo.lnx.warwick.ac.uk>
2017-07-05 15:00:57 +0100

    add terms block to the end of the pages

--- simple-pages/page/show.php
+++ simple-pages/page/show.php
@@ -25,10 +25,20 @@
     $fname = dirname(__FILE__) . '/' . metadata('simple_pages_page', 'slug') . '.php';
     if (is_file( $fname )):
         include( $fname );
-    else :
-
-        echo "this is show.php";// Close your PHP tags and add your show.php content here.
-
+    else:
+?>
+<div class="terms clearfix" style="   position: relative; float: left; clear: both; width: 100%; border-top: 0.214em solid rgb(225, 127, 0);
+    background-color: rgb(50, 78, 107);">
+      <h2 class="editorial-title clearfix" style="position: relative; height: auto; margin: 20px auto;     width: 961px;"><?php echo $title ?>Terms</h2>
+    <div class="terms-contents clearfix" style="position: relative; height: auto; margin: 20px auto;     width: 961px;">
+        <?php
+        $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
+        echo $this->shortcodes($text);
+        ?>
+    </div>
+    <div class"cleafix"></div>
+</div>
+<?php
     endif;
     echo foot();
 
